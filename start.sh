5286dadb612104b1f24669b3c3d321606ddd468d
enghassanashraf <enghassanashraf@gmail.com>
2026-03-16 20:53:46 +0200

    fix: deployment config

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 cd /app/backend
-python manage.py migrate 
-python manage.py migrate_schemas
 python manage.py migrate_schemas --shared
 gunicorn config.wsgi:application --config gunicorn.conf.py
